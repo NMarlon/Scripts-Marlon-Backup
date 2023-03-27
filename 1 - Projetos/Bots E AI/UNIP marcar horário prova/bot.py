31839bac32f19438bc1f551b6590431e65bd9225
Marlon <marlonrviana@gmail.com>
2023-03-27 19:34:49 -0300

    Backup descrição

---
+++ 1 - Projetos/Bots E AI/UNIP marcar horário prova/bot.py
@@ -0,0 +1,28 @@
+"""
+
+
+Cynefin
+
+
+- SIMPLES (Rel. Causa-efeito)
+    - É o que é
+- COMPLICADO (Rel. Causa-efeito, ordenados)
+    - Precisa de um pré-conhecimento, portanto se torna simples sobre esse escopo
+- COMPLEXO (Unknow, ordenado)
+    - Precisa de Análise
+- CAÓTICO (Unknow, impossível conhecer)
+    - Não pode ser conhecido, reconhecer isso, mas tentar transformar para um problema caótico
+
+
+
+
+
+"""
+
+
+import pyautogui
+
+
+
+eye = pyautogui.screenshot()
+# eye.save(r'Path to save screenshot\file name.png')

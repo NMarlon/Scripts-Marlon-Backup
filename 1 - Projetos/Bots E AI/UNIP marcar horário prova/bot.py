51a35e045926c54052d19357854eb78f74c6b61f
Marlon <marlonrviana@gmail.com>
2023-03-28 08:14:25 -0300

    Backup

--- 1 - Projetos/Bots E AI/UNIP marcar horário prova/bot.py
+++ 1 - Projetos/Bots E AI/UNIP marcar horário prova/bot.py
@@ -5,20 +5,20 @@ Cynefin
 
 
 - SIMPLES (Rel. Causa-efeito)
-    - É o que é
+    - Ã‰ o que Ã©
 - COMPLICADO (Rel. Causa-efeito, ordenados)
-    - Precisa de um pré-conhecimento, portanto se torna simples sobre esse escopo
+    - Precisa de um prÃ©-conhecimento, portanto se torna simples sobre esse escopo
 - COMPLEXO (Unknow, ordenado)
-    - Precisa de Análise
-- CAÓTICO (Unknow, impossível conhecer)
-    - Não pode ser conhecido, reconhecer isso, mas tentar transformar para um problema caótico
+    - Precisa de AnÃ¡lise
+- CAÃ“TICO (Unknow, impossÃ­vel conhecer)
+    - NÃ£o pode ser conhecido, reconhecer isso, mas tentar transformar para um problema caÃ³tico
 
 
 
 
 
 """
-
+print("Start")
 
 import pyautogui
 
@@ -26,3 +26,17 @@ import pyautogui
 
 eye = pyautogui.screenshot()
 # eye.save(r'Path to save screenshot\file name.png')
+
+
+
+
+# Find the coordinates of the icon on the desktop
+icon_location = pyautogui.locateOnScreen('./img/teste.png')
+
+# Click on the center of the icon
+if icon_location is not None:
+    icon_center = pyautogui.center(icon_location)
+    pyautogui.click(icon_center)
+    pyautogui.click(icon_center)
+else:
+    print("Icon not found") 

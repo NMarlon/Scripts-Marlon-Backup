4d28cb415d186c685cac90a72a14dbeb22774332
Marlon <marlonrviana@gmail.com>
2023-03-30 21:56:17 -0300

    CSS bem mais ajustado

--- Unip/Semestre8/Site Parque Ambiental/sistema-cantareira/src/index.js
+++ Unip/Semestre8/Site Parque Ambiental/sistema-cantareira/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './css/styles.css';
+ 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,3 +17,17 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+
+/*
+ Bibliografia:
+
+- https://chat.openai.com/ (documentação e como desenvolver em React)
+- https://deepai.org/machine-learning-model/fantasy-world-generator (imagem)
+- 
+
+
+ */

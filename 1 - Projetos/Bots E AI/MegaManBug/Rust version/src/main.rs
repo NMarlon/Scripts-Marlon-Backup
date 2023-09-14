7966f12abaaaa430db53abbd467a9514415d3654
Marlon Rebello Viana <marlonrviana@gmail.com>
2023-09-14 18:36:47 -0300

    Backup

--- 1 - Projetos/Bots E AI/MegaManBug/Rust version/src/main.rs
+++ 1 - Projetos/Bots E AI/MegaManBug/Rust version/src/main.rs
@@ -7,7 +7,7 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::Rect;
-use sdl2::render::TextureCreator;
+use sdl2::render::TextureCreator; 
 use sdl2::video::WindowContext;
 use std::time::Duration;
 
@@ -59,7 +59,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             for x in 0..num_frames_x {
                 let frame = get_frame(&sprite_sheet, x * frame_width, y * frame_height, frame_width, frame_height);
                 let mut texture = texture_creator.create_texture_streaming(pixel_format, frame.width(), frame.height())?;
-                texture.update(None, frame.to_rgba().as_raw(), frame.width() as usize * 4)?;
+                texture.update(None, frame.to_rgba8().as_raw(), frame.width() as usize * 4)?;
                 canvas.copy(&texture, None, Rect::new((x * frame_width) as i32, (y * frame_height) as i32, frame_width, frame_height))?;
             }
         }
---
+++ 1 - Projetos/Bots E AI/MegaManBug/Rust version/target/debug/deps/mega_man.d
@@ -0,0 +1,5 @@
+C:\C De Novo\Scripts-Marlon-Backup\1 - Projetos\Bots E AI\MegaManBug\Rust version\target\debug\deps\mega_man.exe: src\main.rs
+
+C:\C De Novo\Scripts-Marlon-Backup\1 - Projetos\Bots E AI\MegaManBug\Rust version\target\debug\deps\mega_man.d: src\main.rs
+
+src\main.rs:
